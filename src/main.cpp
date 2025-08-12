93bca389aa58e4c458fb7de68b69076bc970a0fb
Samuel Bergé <s19sberg@uni-bonn.de>
2025-08-12 19:10:47 +0200

    Finally got rid of the chaos. Now everything should be understandable. However I deleted the threshold_extension, as i'm going to implement it such that no hit doesn't automatically mean that the edge doesn't get extended (that was the case until now)

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,64 @@
-#include "algorithm.hpp"
+//
+// Created by samuel-berge on 8/11/25.
+//
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "io/chrono_io.hpp"
+#include "io/cl_parser.hpp"
+#include "io/io_functions.hpp"
+#include "core/arrangement.hpp"
+#include "core/edge_extension.hpp"
+#include "core/graph.hpp"
+#include "core/max_flow.hpp"
+#include "utils/logger.hpp"
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello from Edge-Aligned Aggregation!" << std::endl;
-
-    // Require at least 1 argument: input file path
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <input_shapefile_path> [alpha]" << std::endl;
-        return 1;
+void printCommandLineFlags(int argc, char **argv, std::ostream &out = std::cout) {
+    out << "Num args: " << argc << ".\nArgs: ";
+    for (int i = 0; i < argc; i++) {
+        out << argv[i] << ", ";
     }
+    out << std::endl;
+}
 
-    std::string input_path = argv[1];
+int main(int argc, char **argv) {
+    printCommandLineFlags(argc, argv);
+    CLParser clParser(argc, argv);
 
-    // Check if input file exists
-    if (!std::filesystem::exists(input_path)) {
-        std::cerr << "Error: Input file does not exist: " << input_path << std::endl;
-        return 2;
-    }
+    Timer t;
+    t.start();
+    std::cout << "Running algorithm on input file name: " << clParser.inputFileName() << '\n';
+    std::cout << "Alpha is: " << clParser.getAlpha() << '\n';
+    std::cout << "Timelimit is: " << clParser.getTimeLimit() << '\n';
+    std::cout << "Algorithm version is: " << clParser.getVersion() << '\n';
 
-    // Default alpha value
-    double alpha = 0.0003;
+    std::string version = clParser.getVersion();
+    double alpha = clParser.getAlpha();
 
-    // Parse optional alpha argument
-    if (argc >= 3) {
-        try {
-            alpha = std::stod(argv[2]);
-        } catch (const std::invalid_argument& e) {
-            std::cerr << "Error: Invalid alpha value. Must be a number. Received: " << argv[2] << std::endl;
-            return 3;
-        } catch (const std::out_of_range& e) {
-            std::cerr << "Error: Alpha value out of range. Received: " << argv[2] << std::endl;
-            return 4;
-        }
-    }
+    auto data = SHPLoader::ReadShapeFileToPoint2D(clParser.inputFileName());
 
-    std::cout << "Alpha is: " << alpha << std::endl;
+    std::vector<Segment_w_info> input_segments= IO_FUNCTIONS::read_in_segment(data.first, data.second);
 
-    algorithm algo;
-    algo.run(input_path, alpha);
-    std::string output_path = "/home/samuel-berge/Work/bachelorsthesis/build/shp/shp_output.shp";
-    std::string command1 = "qgis " + input_path + " &";
-    std::string command2 = "qgis " + output_path + " &";
+    EDGE_EXTENSION::add_outer_box(input_segments);
+
+    std::vector<Segment_w_info> extended_segments = EDGE_EXTENSION::edge_extension(input_segments, version);
+
+    Arrangement arr = ARRANGEMENT::build_arrangement(extended_segments);
+
+    Graph graph = GRAPH::build_graph(arr, alpha);
+
+    std::vector<bool> max_flow_solution = MAX_FLOW::max_flow(graph, arr);
+
+    std::vector<Polygon_with_holes_2> output_data = IO_FUNCTIONS::combine_polygons(max_flow_solution, arr);
+
+    IO_FUNCTIONS::write_to_shp(output_data, clParser.outputFileName());
+
+    std::string command1 = "qgis " + clParser.inputFileName() + " &";
+    std::string command2 = "qgis " + clParser.outputFileName() + " &";
     int i = std::system(command1.c_str());
     int j = std::system(command2.c_str());
 
-    return 0;
+    std::cout << "Time taken overall: " << t.elapsed() << '\n';
+    return EXIT_SUCCESS;
 }
+
